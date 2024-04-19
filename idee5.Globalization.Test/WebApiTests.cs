06ee4d96e0ecebbc4e4e5530c9690d680b00809c
Bernd Dongus <bernd.dongus@idee5.com>
2024-04-19 07:06:26 +0200

    Deserialization of ResourceTranslations

---
+++ idee5.Globalization.Test/WebApiTests.cs
@@ -0,0 +1,27 @@
+﻿using idee5.Globalization.Models;
+
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace idee5.Globalization.Test;
+[TestClass]
+public class WebApiTests {
+    [TestMethod]
+    public async Task CanReadResourceTranslations() {
+        // Arrange
+        await using var webapp = new WebApplicationFactory<Web.Startup>();
+        using HttpClient client = webapp.CreateClient();
+
+        // Act
+        var response = await client.GetFromJsonAsync<ResourceTranslations>("/api/query/GetResourceKeyTranslations?ResourceSet=CommonTerms&Id=Maybe");
+
+        // Assert
+        Assert.IsNotNull(response);
+        Assert.AreEqual("CommonTerms", response.ResourceSet);
+        Assert.AreEqual(3, response.Translations.Length);
+    }
+}
--- idee5.Globalization/Models/ResourceTranslations.cs
+++ idee5.Globalization/Models/ResourceTranslations.cs
@@ -5,6 +5,17 @@ namespace idee5.Globalization.Models;
 /// The resource translations
 /// </summary>
 public record ResourceTranslations : ResourceKey {
+    public ResourceTranslations() {
+        Translations = [];
+    }
+    public ResourceTranslations(string resourceSet, string id, string? industry, string? customer, Translation[] translations) {
+        ResourceSet = resourceSet;
+        Id = id;
+        Industry = industry;
+        Customer = customer;
+        Translations = translations;
+    }
+
     public ResourceTranslations(ResourceKey original, Translation[] translations) : base(original) {
         Translations = translations ?? throw new ArgumentNullException(nameof(translations));
     }

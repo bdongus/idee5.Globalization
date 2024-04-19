20809c51b26d0b7edf459d4f800cf6bc1ffecbdc
Bernd Dongus <bernd.dongus@idee5.com>
2024-04-19 10:06:59 +0200

    mutable translations

--- idee5.Globalization/Models/Translation.cs
+++ idee5.Globalization/Models/Translation.cs
@@ -3,7 +3,30 @@
 /// <summary>
 /// The translation
 /// </summary>
-/// <param name="Language">Language id according to BCP 47 http://tools.ietf.org/html/bcp47</param>
-/// <param name="Value">Value of the resource for the specified culture and parlance</param>
-/// <param name="Comment">Additional information. Mostly used to create semantic context to simplify translations</param>
-public record Translation(string Language, string Value, string? Comment = null);
+public record Translation {
+    /// <summary>
+    /// Create a new translation
+    /// </summary>
+    /// <param name="language">Language id according to BCP 47 http://tools.ietf.org/html/bcp47</param>
+    /// <param name="value">Value of the resource for the specified culture and parlance</param>
+    /// <param name="comment">Additional information. Mostly used to create semantic context to simplify translations</param>
+    public Translation(string language, string value, string? comment = null) {
+        Language = language;
+        Value = value;
+        Comment = comment;
+    }
+    /// <summary>
+    /// Language id according to BCP 47 http://tools.ietf.org/html/bcp47
+    /// </summary>
+    public string Language { get; set; }
+
+    /// <summary>
+    /// Value of the resource for the specified culture and parlance
+    /// </summary>
+    public string Value { get; set; }
+
+    /// <summary>
+    /// Additional information. Mostly used to create semantic context to simplify translations
+    /// </summary>
+    public string? Comment { get; set; }
+}

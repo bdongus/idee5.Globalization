5c52dbd8b3981fc8842c298a54acb328380c4a95
Bernd Dongus <bernd.dongus@idee5.com>
2024-04-20 20:20:03 +0200

    Additional contrustors for CreateOrUpdateResourceKeyCommand

--- idee5.Globalization/Commands/CreateOrUpdateResourceKeyCommand.cs
+++ idee5.Globalization/Commands/CreateOrUpdateResourceKeyCommand.cs
@@ -1,19 +1,15 @@
 ﻿using idee5.Globalization.Models;
 
-using System;
-using System.Collections.Immutable;
+using System.Collections.Generic;
 
 namespace idee5.Globalization.Commands;
 /// <summary>
 /// The update resource key command
 /// </summary>
-public record CreateOrUpdateResourceKeyCommand : ResourceKey {
-    public CreateOrUpdateResourceKeyCommand(ResourceKey original, ImmutableList<Translation> translations) : base(original) {
-        Translations = translations ?? throw new ArgumentNullException(nameof(translations));
+public record CreateOrUpdateResourceKeyCommand : ResourceTranslations {
+    public CreateOrUpdateResourceKeyCommand(ResourceKey original, IList<Translation> translations) : base(original, translations) {
     }
 
-    /// <summary>
-    /// Translations of the <see cref="ResourceKey">.
-    /// </summary>
-    public ImmutableList<Translation> Translations { get; set; }
+    public CreateOrUpdateResourceKeyCommand(string resourceSet, string id, string? industry, string? customer, IList<Translation> translations) : base(resourceSet, id, industry, customer, translations) {
+    }
 }
